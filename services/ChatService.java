ef99881881dcbbcba3f7503790866a9fdbae1af5
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-05-01 08:45:33 +0530

    Defined tokenize method that tokenizes text into lower case, filtered words

--- services/ChatService.java
+++ services/ChatService.java
@@ -8,6 +8,13 @@ import exceptions.AnswerNotFoundException;
 
 
 public class ChatService {
-
+    private List<String> tokenize(String text) {
+        if (text == null) throw new NullPointerException("Text cannot be null.");
+        return Arrays.stream(text.toLowerCase()
+                        .replaceAll("[^a-zA-Z0-9 ]", " ")
+                        .split("\\s+"))
+                .filter(w -> w.length() > 2)
+                .collect(Collectors.toList());
+    }
 
 }
