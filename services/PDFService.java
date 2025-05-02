27e4353df2c2c2689f98412cb3906e1e85f6a94e
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-05-02 14:59:13 +0530

    Intialised PDFService.java

---
+++ services/PDFService.java
@@ -0,0 +1,17 @@
+package services;
+
+import exceptions.EmptyPDFException;
+import exceptions.InvalidPDFException;
+import models.PDFDocument;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class PDFService {
+
+
+}
