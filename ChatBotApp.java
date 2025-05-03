a7990397328a87be6a262053f1f95e26eed6e15a
Vedant Thakur <vedant.thakur.btech2023@sitpune.edu.in>
2025-05-03 10:45:09 +0530

    Defined Functionality of Main Method 'ChatBotApp' that class the UserInteractionServices in order to start up the bot

--- ChatBotApp.java
+++ ChatBotApp.java
@@ -3,5 +3,7 @@ import services.UserInteractionService;
 public class ChatBotApp {
     public static void main(String[] args) {
 
+        UserInteractionService uiService = new UserInteractionService();
+        uiService.start();
     }
 }
