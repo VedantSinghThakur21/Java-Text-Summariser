3092f9fba3199e9fa065a440dbeee9884643e58a
codeneat9 <yashwithbehara18@gmail.com>
2025-05-03 10:35:39 +0530

    Added comments to make the code more readable and understandable

--- models/PDFDocument.java
+++ models/PDFDocument.java
@@ -1,33 +1,33 @@
 package models;
 
 public class PDFDocument {
-    private String fileName;  
-    private String content;
+    private String fileName;  // Name of the file (also used as the file path)
+    private String content;   // Content of the PDF document
 
-    public PDFDocument() {}
-    public PDFDocument(String fileName, String content) {
+    public PDFDocument() {} // Default constructor
+    public PDFDocument(String fileName, String content) {  // Constructor with parameters
         this.fileName = fileName;
         this.content = content;
     }
-    public String getFileName() {
+    public String getFileName() {  // Returns the file name
         return fileName;
     }
-    public String getFilePath() {
+    public String getFilePath() {  // Returns the file path (same as file name in this case)
         return fileName; 
     }
 
-    public String getContent() {
+    public String getContent() {  // Returns the content of the PDF document
         return content;
     }
 
-    public void setFileName(String fileName) {
+    public void setFileName(String fileName) {  // Sets the file name
         this.fileName = fileName;
     }
 
-    public void setFilePath(String filePath) {
+    public void setFilePath(String filePath) {  // Sets the file path (same as file name in this case)
         this.fileName = filePath; 
     }
-    public void setContent(String content) {
+    public void setContent(String content) {  // Sets the content of the PDF document
         this.content = content;
     }
 }
